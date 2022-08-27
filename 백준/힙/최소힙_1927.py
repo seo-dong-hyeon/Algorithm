4f4b11bf3c2644e2717f8d643536880d20a30601
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2022-08-28 05:22:01 +0900

    20220827 - 백준_힙_2문제

---
+++ 백준/힙/최소힙_1927.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+N = int(input())
+heap = []
+
+for i in range(N):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
+    else:
+        heapq.heappush(heap, num)
